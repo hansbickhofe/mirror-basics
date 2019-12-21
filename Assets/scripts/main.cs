92990870df31ed8c8aca70658fe8c8abd2d00c80
Hans Bickhofe <hb@niwo.tv>
2019-12-21 23:09:36 +0100

    refresh server list 1

--- Assets/scripts/main.cs
+++ Assets/scripts/main.cs
@@ -1,70 +1,104 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Assets.Scripts.NetworkMessages;
 using Assets.Scripts.Utility.Serialisation;
 using Mirror;
 using UnityEngine;
 
-namespace Mirror {
-    public class main : NetworkBehaviour {
+public class main : MonoBehaviour {
 
-        public NetworkManager NetMngr;
-        Dictionary<string, DiscoveryInfo> m_discoveredServers = new Dictionary<string, DiscoveryInfo> ();
+    public NetworkManager NetMngr;
+    Dictionary<string, DiscoveryInfo> m_discoveredServers = new Dictionary<string, DiscoveryInfo> ();
+    string[] m_headerNames = new string[] { "IP", "Host" };
 
-        // Start is called before the first frame update
-        void Start () {
-            m_discoveredServers.Clear ();
-            NetworkDiscovery.instance.ClientRunActiveDiscovery ();
+    string[] m_gameName = { "Alpha", "Beta", "Gamma", "Delta" };
+
+    void OnEnable () {
+        NetworkDiscovery.onReceivedServerResponse += OnDiscoveredServer;
+    }
+
+    void OnDisable () {
+        NetworkDiscovery.onReceivedServerResponse -= OnDiscoveredServer;
+    }
+
+    // Start is called before the first frame update
+    void Start () {
+        m_discoveredServers.Clear ();
+        NetworkDiscovery.instance.ClientRunActiveDiscovery ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!NetMngr) return;
+        // if (!isLocalPlayer) return;
+        if (Input.GetKeyDown ("s")) StartServer ();
+        if (Input.GetKeyDown ("r")) RefreshServerList ();
+        if (Input.GetKeyDown ("c")) ConnectToFirstServer ();
+        if (Input.GetKeyDown ("d")) DisconnectFromGame ();
+    }
+
+    void StartServer () {
+        int serverNum = (Random.Range (0, 4));
+        if (!NetworkClient.isConnected && !NetworkServer.active) {
+            if (!NetworkClient.active) {
+                print ("Start Gameserver-" + m_gameName[serverNum]);
+                // LAN Host
+                m_discoveredServers.Clear ();
+                NetworkManager.singleton.StartHost ();
+
+                // Wire in broadcaster pipeline here
+                GameBroadcastPacket gameBroadcastPacket = new GameBroadcastPacket ();
+
+                gameBroadcastPacket.serverAddress = NetworkManager.singleton.networkAddress;
+                gameBroadcastPacket.port = ((TelepathyTransport) Transport.activeTransport).port;
+                gameBroadcastPacket.hostName = "Gameserver-" + m_gameName[serverNum];
+                gameBroadcastPacket.serverGUID = NetworkDiscovery.instance.serverId;
+
+                byte[] broadcastData = ByteStreamer.StreamToBytes (gameBroadcastPacket);
+                NetworkDiscovery.instance.ServerPassiveBroadcastGame (broadcastData);
+            }
         }
+    }
 
-        // Update is called once per frame
-        void Update () {
-            if (!NetMngr) return;
-            if (!isLocalPlayer) return;
+    void RefreshServerList () {
+        NetworkDiscovery.instance.ClientRunActiveDiscovery ();
+        print ("RefreshServerList");
 
-#if UNITY_EDITOR || UNITY_STANDALONE_WIN
-
-            if (Input.GetKeyDown ("s")) StartServer ();
-            if (Input.GetKeyDown ("r")) RefreshServerList ();
-            if (Input.GetKeyDown ("c")) ConnectToFirstServer ();
-            if (Input.GetKeyDown ("d")) DisconnectFromGame ();
-
-#endif
-
-            void StartServer () {
-                if (!NetworkClient.isConnected && !NetworkServer.active) {
-                    if (!NetworkClient.active) {
-                        // LAN Host
-                        if (GUILayout.Button ("Passive Host", GUILayout.Height (25), GUILayout.ExpandWidth (false))) {
-                            m_discoveredServers.Clear ();
-                            NetworkManager.singleton.StartHost ();
-
-                            // Wire in broadcaster pipeline here
-                            GameBroadcastPacket gameBroadcastPacket = new GameBroadcastPacket ();
-
-                            gameBroadcastPacket.serverAddress = NetworkManager.singleton.networkAddress;
-                            gameBroadcastPacket.port = ((TelepathyTransport) Transport.activeTransport).port;
-                            gameBroadcastPacket.hostName = "MyDistinctDummyPlayerName";
-                            gameBroadcastPacket.serverGUID = NetworkDiscovery.instance.serverId;
-
-                            byte[] broadcastData = ByteStreamer.StreamToBytes (gameBroadcastPacket);
-                            NetworkDiscovery.instance.ServerPassiveBroadcastGame (broadcastData);
-                        }
-                    }
+        foreach (var info in m_discoveredServers.Values) {
+            print ("Info " + info.unpackedData.serverGUID);
+            print ("Info " + info.unpackedData.hostName);
+            print ("Info " + info.unpackedData.serverAddress);
+            
+            for (int i = 0; i < m_headerNames.Length; i++) {
+                if (i == 0) {
+                    print ("server Name: " + info.unpackedData.hostName + " " + " GUID: " + info.unpackedData.serverGUID +
+                        " server Address: " + info.unpackedData.serverAddress);
                 }
             }
-
-            void RefreshServerList () {
-
-            }
-
-            void ConnectToFirstServer () {
-
-            }
-
-            void DisconnectFromGame () {
-
-            }
         }
+
+        m_discoveredServers.Clear ();
+    }
+
+    void ConnectToFirstServer () {
+        print ("ConnectToFirstServer ");
+
+        if (NetworkManager.singleton == null ||
+            Transport.activeTransport == null) {
+            return;
+        }
+        if (!(Transport.activeTransport is TelepathyTransport)) {
+            Debug.LogErrorFormat ("Only {0} is supported", typeof (TelepathyTransport));
+            return;
+        }
+
+    }
+
+    void DisconnectFromGame () {
+        print ("DisconnectFromGame");
+    }
+
+    void OnDiscoveredServer (DiscoveryInfo info) {
+        // Note that you can check the versioning to decide if you can connect to the server or not using this method
+        m_discoveredServers[info.unpackedData.serverGUID] = info;
     }
 }
