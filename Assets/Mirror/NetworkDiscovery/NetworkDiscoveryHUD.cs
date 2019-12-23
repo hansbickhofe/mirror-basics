55a8eb0e9cca08d6bc6fbe1a595a9b6532f1ac29
Hans Bickhofe <hb@niwo.tv>
2019-12-23 22:48:30 +0100

    last commit - 3sec refresh on awake l for list servers

--- Assets/Mirror/NetworkDiscovery/NetworkDiscoveryHUD.cs
+++ Assets/Mirror/NetworkDiscovery/NetworkDiscoveryHUD.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mirror {
@@ -16,35 +13,10 @@ namespace Mirror {
         };
         string[] m_gameName = { "Alpha", "Beta", "Gamma", "Delta" };
 
-        Vector2 m_scrollViewPos = Vector2.zero;
+        public System.Action<NetworkDiscovery.DiscoveryInfo> connectAction;
         public bool IsRefreshing { get { return Time.realtimeSinceStartup - m_timeWhenRefreshed < this.refreshInterval; } }
         float m_timeWhenRefreshed = 0f;
-        bool m_displayBroadcastAddresses = false;
-
-        IPEndPoint m_lookupServer = null; // server that we are currently looking up
-        string m_lookupServerIP = "";
-        string m_lookupServerPort = NetworkDiscovery.kDefaultServerPort.ToString ();
-        float m_timeWhenLookedUpServer = 0f;
-        bool IsLookingUpAnyServer {
-            get {
-                return Time.realtimeSinceStartup - m_timeWhenLookedUpServer < this.refreshInterval &&
-                    m_lookupServer != null;
-            }
-        }
-
-        GUIStyle m_centeredLabelStyle;
-
-        public bool drawGUI = true;
-        public int offsetX = 5;
-        public int offsetY = 150;
-        public int width = 500, height = 400;
         [Range (1, 5)] public float refreshInterval = 3f;
-
-        public System.Action<NetworkDiscovery.DiscoveryInfo> connectAction;
-
-        NetworkDiscoveryHUD () {
-            this.connectAction = this.Connect;
-        }
 
         void OnEnable () {
             NetworkDiscovery.onReceivedServerResponse += OnDiscoveredServer;
@@ -54,28 +26,13 @@ namespace Mirror {
             NetworkDiscovery.onReceivedServerResponse -= OnDiscoveredServer;
         }
 
-        void Start () {
-
+        void Awake () {
+            InvokeRepeating(nameof(Refresh),0,3);
         }
-
-        // void OnGUI () {
-
-        //     if (null == m_centeredLabelStyle) {
-        //         m_centeredLabelStyle = new GUIStyle (GUI.skin.label);
-        //         m_centeredLabelStyle.alignment = TextAnchor.MiddleCenter;
-        //     }
-
-        //     if (this.drawGUI)
-        //         this.Display (new Rect (offsetX, offsetY, width, height));
-
-        // }
 
         void Update () {
             if (Input.GetKeyDown ("s")) StartServer ();
-            if (Input.GetKeyDown ("r")) {
-                Refresh ();
-                DisplayServers ();
-            }
+            if (Input.GetKeyDown ("l")) DisplayServers ();
             if (Input.GetKeyDown ("c")) ConnectToFirstServer ();
             if (Input.GetKeyDown ("d")) DisconnectFromGame ();
         }
@@ -105,97 +62,25 @@ namespace Mirror {
             else NetworkManager.singleton.StopHost ();
         }
 
-        // public void Display (Rect displayRect) {
-        //     if (null == NetworkManager.singleton)
-        //         return;
-        //     if (NetworkServer.active || NetworkClient.active)
-        //         return;
-        //     if (!NetworkDiscovery.SupportedOnThisPlatform)
-        //         return;
-
-        //     // GUILayout.BeginArea (displayRect);
-
-        //     // this.DisplayRefreshButton ();
-
-        //     // lookup a server
-
-        //     // GUILayout.Label ("Lookup server: ");
-        //     // GUILayout.BeginHorizontal ();
-        //     // GUILayout.Label ("IP:");
-        //     m_lookupServerIP = GUILayout.TextField (m_lookupServerIP, GUILayout.Width (120));
-        //     // GUILayout.Space (10);
-        //     // GUILayout.Label ("Port:");
-        //     m_lookupServerPort = GUILayout.TextField (m_lookupServerPort, GUILayout.Width (60));
-        //     // GUILayout.Space (10);
-        //     if (IsLookingUpAnyServer) {
-        //         GUILayout.Button ("Lookup...", GUILayout.Height (25), GUILayout.MinWidth (80));
-        //     } else {
-        //         if (GUILayout.Button ("Lookup", GUILayout.Height (25), GUILayout.MinWidth (80)))
-        //             LookupServer ();
-        //     }
-        //     GUILayout.FlexibleSpace ();
-        //     GUILayout.EndHorizontal ();
-
-        //     GUILayout.BeginHorizontal ();
-        //     m_displayBroadcastAddresses = GUILayout.Toggle (m_displayBroadcastAddresses, "Display broadcast addresses", GUILayout.ExpandWidth (false));
-        //     if (m_displayBroadcastAddresses) {
-        //         GUILayout.Space (10);
-        //         GUILayout.Label (string.Join (", ", NetworkDiscovery.GetBroadcastAdresses ().Select (ip => ip.ToString ())));
-        //     }
-        //     GUILayout.EndHorizontal ();
-
-        //     GUILayout.Label (string.Format ("Servers [{0}]:", m_discoveredServers.Count));
-
-        //     this.DisplayServers ();
-
-        //     GUILayout.EndArea ();
-
-        // }
-
-        // public void DisplayRefreshButton () {
-        //     if (IsRefreshing) {
-        //         GUILayout.Button ("Refreshing...", GUILayout.Height (25), GUILayout.ExpandWidth (false));
-        //     } else {
-        //         if (GUILayout.Button ("Refresh LAN", GUILayout.Height (25), GUILayout.ExpandWidth (false))) {
-        //             Refresh ();
-        //         }
-        //     }
-        // }
-
         public void DisplayServers () {
-
-            // int elemWidth = this.width / m_headerNames.Length - 5;
-
-            // header
-            // GUILayout.BeginHorizontal ();
-            // foreach (string str in m_headerNames) {
-            //     // print (str);
-            // }
-
             foreach (var info in m_discoveredServers) {
-                // if (GUILayout.Button (info.EndPoint.Address.ToString (), GUILayout.Width (elemWidth)))
-                //     this.connectAction (info);
-
+                string hostline = "Gameinfo: " + info.EndPoint.Address.ToString ();
                 for (int i = 1; i < m_headerNames.Length; i++) {
                     if (info.KeyValuePairs.ContainsKey (m_headerNames[i])) {
-                        print (info.KeyValuePairs[m_headerNames[i]]);
-                        // GUILayout.Label (info.KeyValuePairs[m_headerNames[i]], m_centeredLabelStyle, GUILayout.Width (elemWidth));
+                        hostline += " ";
+                        hostline += info.KeyValuePairs[m_headerNames[i]];
                     } else print ("empty");
                 }
+                print (hostline);
             }
         }
 
         public void Refresh () {
+            print("refresh");
             m_discoveredServers.Clear ();
             m_timeWhenRefreshed = Time.realtimeSinceStartup;
             NetworkDiscovery.SendBroadcast ();
-
-        }
-
-        bool IsLookingUpServer (IPEndPoint endPoint) {
-            return Time.realtimeSinceStartup - m_timeWhenLookedUpServer < this.refreshInterval &&
-                m_lookupServer != null &&
-                m_lookupServer.Equals (endPoint);
+            DisplayServers ();
         }
 
         void Connect (NetworkDiscovery.DiscoveryInfo info) {
@@ -216,10 +101,6 @@ namespace Mirror {
         }
 
         void OnDiscoveredServer (NetworkDiscovery.DiscoveryInfo info) {
-            print ("OnDiscoveredServer " + info.EndPoint.ToString ());
-            if (!IsRefreshing && !IsLookingUpServer (info.EndPoint))
-                return;
-
             int index = m_discoveredServers.FindIndex (item => item.EndPoint.Equals (info.EndPoint));
             if (index < 0) {
                 // server is not in the list
