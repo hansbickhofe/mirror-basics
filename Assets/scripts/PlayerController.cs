957245df77b8c93c155c2e63358a484888481c53
Hans Bickhofe <hb@niwo.tv>
2020-01-16 20:15:45 +0100

    updated player controller

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : NetworkBehaviour {
     public float Speed = 5f;
 
     void Update () {
+        if (!isLocalPlayer) return;
         float h = Input.GetAxisRaw ("Horizontal");
 
         GetComponent<Rigidbody> ().velocity = new Vector2 (h, 0) * Speed;
